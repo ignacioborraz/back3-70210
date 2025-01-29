291235371e84427ee32750c6119345f3876c4fb1
ignacioborraz <ignacioborraz@hotmail.com>
2025-01-28 22:27:42 -0300

    cluster

---
+++ dockerfile
@@ -0,0 +1,16 @@
+# Selecciona una imagen base
+FROM node
+
+# Establece el directorio de trabajo
+WORKDIR /coder-server
+
+# Copia los archivos de la aplicación
+COPY package.json ./
+RUN npm install
+COPY . .
+
+# Expone el puerto de la aplicación
+EXPOSE 8080
+
+# Comando para ejecutar la aplicación
+CMD ["npm", "run", "dev"]
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cluster from "cluster";
+import { cpus } from "os";
 import dbConnect from "./src/utils/dbConnect.util.js";
 import argsUtil from "./src/utils/args.util.js";
 import router from "./src/routers/index.router.js";
@@ -13,17 +15,32 @@ const server = express();
 const port = env.PORT || 8080;
 const ready = async () => {
   const mode = argsUtil.mode;
-  loggerUtil.INFO("server ready on " + mode + " mode and on port " + port);
+  loggerUtil.INFO(
+    `server ready on ${mode} mode, on port ${port} and on process ${process.pid}`
+  );
   await dbConnect();
 };
+/* SOLO los procesos primarios pueden crear WORKERS */
+//loggerUtil.INFO(JSON.stringify({ isPrimary: cluster.isPrimary, numberOfProcess: cpus().length }))
+const isPrimary = cluster.isPrimary;
+const numberOfProcess = cpus().length;
+if (isPrimary) {
+  loggerUtil.INFO("isPrimary: " + process.pid);
+  for (let index = 1; index <= numberOfProcess; index++) {
+    cluster.fork();
+  }
+  // el proceso primario NO LEVANTA EL SERVIDOR
+  // solamente crea los nodos
+  //server.listen(port, ready);
+} else {
+  loggerUtil.INFO("isWorker: " + process.pid);
+  server.listen(port, ready);
+}
 
 server.use(compression({ brotli: { enabled: true, zlib: {} } }));
 server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(httpLogger)
-
-server.listen(port, ready);
-
+server.use(httpLogger);
 server.use("/api", router);
-server.use(errorHandler)
+server.use(errorHandler);
